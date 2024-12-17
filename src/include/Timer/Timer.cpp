726e3bea9ed5c8e55cbc261fbedc19673f1ce94c
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2024-12-17 13:15:45 +0100

    fix: TimeElapsKey already Exist.

--- src/include/Timer/Timer.cpp
+++ src/include/Timer/Timer.cpp
@@ -1,34 +1,26 @@
 #include "Timer.h"
 #include "raylib.h"
-#include <fstream>
 #include <ctime>
+#include <fstream>
 
+Timer::Timer() { this->elapsedTime = 0; }
 
-Timer::Timer(){
-  this->elapsedTime = 0;
-}
+Timer::~Timer() { this->elapsedTime = 0; }
 
-Timer::~Timer(){
-  this->elapsedTime = 0;
-}
+void Timer::startTimer() { this->elapsedTime = 0; }
 
-void Timer::startTimer() { this->elapsedTime = 0;
-                          }
-
-void Timer::UpdateTimer() {
-  this->elapsedTime+= GetFrameTime();
-};
-void Timer::stopTimer(){
+void Timer::UpdateTimer() { this->elapsedTime += GetFrameTime(); };
+void Timer::stopTimer() {
   // Get the current time as a unique identifier
   std::time_t now = std::time(nullptr);
   char timeStr[20];
   std::strftime(timeStr, sizeof(timeStr), "%Y%m%d%H%M%S", std::localtime(&now));
 
   // Save the elapsed time to a file with a unique key
-  std::ofstream file("./data.ini", std::ios::app);
-  if (file.is_open()) {
-    file << "ElapsedTime_" << timeStr << "=" << elapsedTime << "\n";
-    file.close();
-  }
+  // std::ofstream file("./data.ini", std::ios::app);
+  // if (file.is_open()) {
+  //   file << "ElapsedTime_" << timeStr << "=" << elapsedTime << "\n";
+  //   file.close();
+  // }
 }
-float Timer::getElapsedTime(){ return elapsedTime;}
+float Timer::getElapsedTime() { return elapsedTime; }
